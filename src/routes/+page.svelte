6cf4b0c27984f25093d13b49e339ca3ded6c1407
Prathik Murthy <murthypr@umich.edu>
2023-03-23 03:04:27 -0400

    supabase init

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,13 +3,12 @@
 </script>
 
 <style>
-    p {
-        color:green
-    }
 </style>
 
-<h1>Welcome to SvelteKit</h1>
-<h1>{count}</h1>
-<button on:click={() => count++}>Increment</button>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
-<a href="/about">About Page</a>
+<main>
+    <h1>Welcome to SvelteKit</h1>
+    <h1>{count}</h1>
+    <button on:click={() => count++}>Increment</button>
+    <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+    <a href="/about">About Page</a>
+</main>
