5322898bf11fa1a21b622cfb410dbb1a2cdfc518
Prathik Murthy <murthypr@umich.edu>
2023-03-23 03:04:34 -0400

    add supabase

---
+++ src/lib/supabase.ts
@@ -0,0 +1,11 @@
+import { PUBLIC_SUPABASE_URL, PUBLIC_SUPABASE_KEY } from "$env/static/public";
+import { createClient } from "@supabase/supabase-js";
+import { writable } from "svelte/store";
+
+export const supabase = createClient( `${PUBLIC_SUPABASE_URL}`, `${PUBLIC_SUPABASE_KEY}` );
+
+export const currentUser = writable((await supabase.auth.getUser()).data.user);
+
+supabase.auth.onAuthStateChange(async () => {
+    currentUser.set((await supabase.auth.getUser()).data.user);
+});
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,14 +1,72 @@
 <script lang="ts">
-    let count = 0;
+
+    import { supabase, currentUser } from '../lib/supabase';
+
+    let email = '';
+    let password = '';
+
+        
+    // check if the user is logged in
+    // onMount(async () => {
+    //     const { data, error } = await supabase.auth.getSession();
+
+    //     if (error) {
+    //         console.log(error);
+    //     } else {
+    //         console.log(data);
+    //     }
+    // });
+
+    const login = async (email: string, password: string) => {
+        // log in the user
+        const { data, error } = await supabase.auth.signInWithPassword({
+            email: email,
+            password: password,
+        })
+
+        if (error) {
+            console.log(error);
+        } else {
+            console.log(data);
+        }
+    };
+
+    const createAccount = async (email: string, password: string) => {
+        // create a new user
+        const { data, error } = await supabase.auth.signUp({
+            email: email,
+            password: password,
+        });
+
+        if (error) {
+            console.log(error);
+        } else {
+            console.log("Account created!");
+            currentUser.set(data?.user);
+        }
+    };
 </script>
 
+<!-- make the buttons look nice -->
 <style>
+  button {
+    background: #eee;
+    border: 1px solid #ccc;
+    border-radius: 4px;
+    padding: 0.5em 1em;
+    margin: 0.5em;
+  }
 </style>
 
-<main>
-    <h1>Welcome to SvelteKit</h1>
-    <h1>{count}</h1>
-    <button on:click={() => count++}>Increment</button>
-    <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
-    <a href="/about">About Page</a>
-</main>
+<!-- create input fields for the buttons -->
+<input type="text" placeholder="Email" bind:value={email} />
+<input type="password" placeholder="Password" bind:value={password}/>
+<!-- buttons which use the fields -->
+<button on:click={() => login(email, password)}>Login</button>
+<button on:click={() => createAccount(email, password)}>Create Account</button>
+<!-- logout button -->
+<button on:click={() => supabase.auth.signOut()}>Logout</button>
+
+<!-- display the user's email -->
+<p>{$currentUser?.email == undefined ? "Not Logged In" : $currentUser.email}</p>
+
